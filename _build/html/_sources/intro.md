15d4f818949481c48fbbd17c710c34eca57ddd04
UmbraVenus <60438078+UmbraVenus@users.noreply.github.com>
2021-07-14 09:06:08 -0700

    added recommendations

--- _build/html/_sources/intro.md
+++ _build/html/_sources/intro.md
@@ -1,1 +1,7 @@
 # Welcome to Deviant Art and Augmented Reality by Sage
+
+Welcome to deviant art pages
+
+Here you will see each day's recommended collections
+
+Then we will use Augmented Reality to show you how it looks in the picture of your choosing.
--- _build/jupyter_execute/notebooks.py
+++ _build/jupyter_execute/notebooks.py
@@ -3,6 +3,8 @@
 
 # # Daily Deviations
 
+# ## ArtWork Updated Daily ##
+
 # In[1]:
 
 
@@ -17,44 +19,50 @@ import argparse
 import imutils
 import sys
 import cv2
+import ipywidgets as widgets
+from IPython.display import display
 
 
 # In[2]:
 
 
-
 da = deviantart.Api("16260","66397f4afec59514bdf212884f4e0d74")
 dailydeviations = da.browse_dailydeviations()
+
+dalist = []
+counter = 1
 for deviation in dailydeviations:
-#print(da.get_deviation_metadata(deviation.deviationid)[0]["description"])
-#print(deviation.content)
+    #print(da.get_deviation_metadata(deviation.deviationid)[0]["description"])
+    #print(deviation.content)
     
     jr = deviation.content
-#for key, value in jr.items():
-    #  print(key, ":", value)
+    #for key, value in jr.items():
+      #  print(key, ":", value)
     if jr:
+        #print(jr["src"])
 
         response = requests.get(jr["src"],stream=True)
-        img = Image.open(response.raw)
-
-        plt.imshow(img)
-        plt.show()
+        #img = Image.open(response.raw)
+        image = widgets.Image(value=response.raw.read(),format="png",width=400,height=400)
+        
+        text = widgets.Label(
+            value = str(counter),
+        )
+        items = widgets.HBox([image,text])
+        #plt.imshow(img)
+        #plt.show()
+        dalist += [items]
+        counter += 1
+widgets.GridBox(dalist, layout=widgets.Layout(grid_template_columns="repeat(2, 500px)"))
 
 
 # In[3]:
 
 
-resp = requests.get("https://backend.deviantart.com/oembed?url=http%3A%2F%2Ffav.me%2Fd2enxz7")
-if resp.status_code != 200:
-    raise ApiErrpr('Get /tasks/ {}'.format(resp.status_code))
-jr = resp.json()
-for key, value in jr.items():
-    print(key, ":", value)
-print(jr["url"])
-
-response = requests.get(jr["url"],stream=True)
-img = Image.open(response.raw)
-
-plt.imshow(img)
-plt.show()
+radio = widgets.RadioButtons(
+            options = [x for x in range(1,13)],
+            description="Your AR Pick:",
+            disabled = False
+        )
+radio
 
