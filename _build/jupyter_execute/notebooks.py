17558238882299d014a462f8663b39bc6353b5ab
UmbraVenus <60438078+UmbraVenus@users.noreply.github.com>
2021-07-14 10:45:58 -0700

    add

--- _build/jupyter_execute/notebooks.py
+++ _build/jupyter_execute/notebooks.py
@@ -57,23 +57,267 @@ for deviation in dailydeviations:
 widgets.GridBox(dalist, layout=widgets.Layout(grid_template_columns="repeat(2, 320px)"))
 
 
-# ## Choose your fav one and get more recommendations! :D ##
+# ## More Recommendations ##
+
+# ### 1st artist ###
 
 # In[3]:
 
 
-radio = widgets.RadioButtons(
-            options = [x for x in range(1,13)],
-            description="Your AR Pick:",
-            disabled = False
-        )
-radio
+morefav = da.browse_morelikethis_preview(dailydeviations[0])['more_from_artist']
+recommendation = []
+for x in morefav:
+    r = x.content
+    #for key, value in jr.items():
+      #  print(key, ":", value)
+    if r:
+        #print(jr["src"])
 
+        response = requests.get(r["src"],stream=True)
+        #img = Image.open(response.raw)
+        image = widgets.Image(value=response.raw.read(),format="png",width=300,height= 300)
+        #plt.imshow(img)
+        #plt.show()
+        recommendation += [image]
+widgets.GridBox(recommendation, layout=widgets.Layout(grid_template_columns="repeat(2, 300px)"))
+
+
+# ### 2nd artist ###
 
 # In[4]:
 
 
-morefav = da.browse_morelikethis_preview(dailydeviations[radio.value])['more_from_artist']
+morefav = da.browse_morelikethis_preview(dailydeviations[1])['more_from_artist']
+recommendation = []
+for x in morefav:
+    r = x.content
+    #for key, value in jr.items():
+      #  print(key, ":", value)
+    if r:
+        #print(jr["src"])
+
+        response = requests.get(r["src"],stream=True)
+        #img = Image.open(response.raw)
+        image = widgets.Image(value=response.raw.read(),format="png",width=300,height= 300)
+        #plt.imshow(img)
+        #plt.show()
+        recommendation += [image]
+widgets.GridBox(recommendation, layout=widgets.Layout(grid_template_columns="repeat(2, 300px)"))
+
+
+# ### 3rd artist ###
+
+# In[5]:
+
+
+morefav = da.browse_morelikethis_preview(dailydeviations[2])['more_from_artist']
+recommendation = []
+for x in morefav:
+    r = x.content
+    #for key, value in jr.items():
+      #  print(key, ":", value)
+    if r:
+        #print(jr["src"])
+
+        response = requests.get(r["src"],stream=True)
+        #img = Image.open(response.raw)
+        image = widgets.Image(value=response.raw.read(),format="png",width=300,height= 300)
+        #plt.imshow(img)
+        #plt.show()
+        recommendation += [image]
+widgets.GridBox(recommendation, layout=widgets.Layout(grid_template_columns="repeat(2, 300px)"))
+
+
+# ### 4th artist ###
+
+# In[6]:
+
+
+morefav = da.browse_morelikethis_preview(dailydeviations[3])['more_from_artist']
+recommendation = []
+for x in morefav:
+    r = x.content
+    #for key, value in jr.items():
+      #  print(key, ":", value)
+    if r:
+        #print(jr["src"])
+
+        response = requests.get(r["src"],stream=True)
+        #img = Image.open(response.raw)
+        image = widgets.Image(value=response.raw.read(),format="png",width=300,height= 300)
+        #plt.imshow(img)
+        #plt.show()
+        recommendation += [image]
+widgets.GridBox(recommendation, layout=widgets.Layout(grid_template_columns="repeat(2, 300px)"))
+
+
+# ### 5th artist ###
+
+# In[7]:
+
+
+morefav = da.browse_morelikethis_preview(dailydeviations[4])['more_from_artist']
+recommendation = []
+for x in morefav:
+    r = x.content
+    #for key, value in jr.items():
+      #  print(key, ":", value)
+    if r:
+        #print(jr["src"])
+
+        response = requests.get(r["src"],stream=True)
+        #img = Image.open(response.raw)
+        image = widgets.Image(value=response.raw.read(),format="png",width=300,height= 300)
+        #plt.imshow(img)
+        #plt.show()
+        recommendation += [image]
+widgets.GridBox(recommendation, layout=widgets.Layout(grid_template_columns="repeat(2, 300px)"))
+
+
+# ### 6th artist ###
+
+# In[8]:
+
+
+morefav = da.browse_morelikethis_preview(dailydeviations[5])['more_from_artist']
+recommendation = []
+for x in morefav:
+    r = x.content
+    #for key, value in jr.items():
+      #  print(key, ":", value)
+    if r:
+        #print(jr["src"])
+
+        response = requests.get(r["src"],stream=True)
+        #img = Image.open(response.raw)
+        image = widgets.Image(value=response.raw.read(),format="png",width=300,height= 300)
+        #plt.imshow(img)
+        #plt.show()
+        recommendation += [image]
+widgets.GridBox(recommendation, layout=widgets.Layout(grid_template_columns="repeat(2, 300px)"))
+
+
+# ### 7th artist ###
+
+# In[9]:
+
+
+morefav = da.browse_morelikethis_preview(dailydeviations[6])['more_from_artist']
+recommendation = []
+for x in morefav:
+    r = x.content
+    #for key, value in jr.items():
+      #  print(key, ":", value)
+    if r:
+        #print(jr["src"])
+
+        response = requests.get(r["src"],stream=True)
+        #img = Image.open(response.raw)
+        image = widgets.Image(value=response.raw.read(),format="png",width=300,height= 300)
+        #plt.imshow(img)
+        #plt.show()
+        recommendation += [image]
+widgets.GridBox(recommendation, layout=widgets.Layout(grid_template_columns="repeat(2, 300px)"))
+
+
+# ### 8th artist ###
+
+# In[10]:
+
+
+morefav = da.browse_morelikethis_preview(dailydeviations[7])['more_from_artist']
+recommendation = []
+for x in morefav:
+    r = x.content
+    #for key, value in jr.items():
+      #  print(key, ":", value)
+    if r:
+        #print(jr["src"])
+
+        response = requests.get(r["src"],stream=True)
+        #img = Image.open(response.raw)
+        image = widgets.Image(value=response.raw.read(),format="png",width=300,height= 300)
+        #plt.imshow(img)
+        #plt.show()
+        recommendation += [image]
+widgets.GridBox(recommendation, layout=widgets.Layout(grid_template_columns="repeat(2, 300px)"))
+
+
+# ### 9th artist ###
+
+# In[11]:
+
+
+morefav = da.browse_morelikethis_preview(dailydeviations[8])['more_from_artist']
+recommendation = []
+for x in morefav:
+    r = x.content
+    #for key, value in jr.items():
+      #  print(key, ":", value)
+    if r:
+        #print(jr["src"])
+
+        response = requests.get(r["src"],stream=True)
+        #img = Image.open(response.raw)
+        image = widgets.Image(value=response.raw.read(),format="png",width=300,height= 300)
+        #plt.imshow(img)
+        #plt.show()
+        recommendation += [image]
+widgets.GridBox(recommendation, layout=widgets.Layout(grid_template_columns="repeat(2, 300px)"))
+
+
+# ### 10th artist ###
+
+# In[12]:
+
+
+morefav = da.browse_morelikethis_preview(dailydeviations[9])['more_from_artist']
+recommendation = []
+for x in morefav:
+    r = x.content
+    #for key, value in jr.items():
+      #  print(key, ":", value)
+    if r:
+        #print(jr["src"])
+
+        response = requests.get(r["src"],stream=True)
+        #img = Image.open(response.raw)
+        image = widgets.Image(value=response.raw.read(),format="png",width=300,height= 300)
+        #plt.imshow(img)
+        #plt.show()
+        recommendation += [image]
+widgets.GridBox(recommendation, layout=widgets.Layout(grid_template_columns="repeat(2, 300px)"))
+
+
+# ### 11th artist ###
+
+# In[ ]:
+
+
+morefav = da.browse_morelikethis_preview(dailydeviations[10])['more_from_artist']
+recommendation = []
+for x in morefav:
+    r = x.content
+    #for key, value in jr.items():
+      #  print(key, ":", value)
+    if r:
+        #print(jr["src"])
+
+        response = requests.get(r["src"],stream=True)
+        #img = Image.open(response.raw)
+        image = widgets.Image(value=response.raw.read(),format="png",width=300,height= 300)
+        #plt.imshow(img)
+        #plt.show()
+        recommendation += [image]
+widgets.GridBox(recommendation, layout=widgets.Layout(grid_template_columns="repeat(2, 300px)"))
+
+
+# ### 12th artist ###
+
+# In[ ]:
+
+
+morefav = da.browse_morelikethis_preview(dailydeviations[11])['more_from_artist']
 recommendation = []
 for x in morefav:
     r = x.content
