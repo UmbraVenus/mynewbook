187c12544c4598a6f90e5c42f4459ea3b1d1e2d6
UmbraVenus <60438078+UmbraVenus@users.noreply.github.com>
2021-07-20 12:47:22 -0700

    added deep dream

---
+++ _build/jupyter_execute/deepdream.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Deep Dream of Deviation #
+
+# ### Applying machine learning and deep dream into selected image ###
+
+# In[1]:
+
+
+get_ipython().run_cell_magic('html', '', '<script src="https://gist.github.com/UmbraVenus/de580771a7f2d4870691b57c6b9209c3.js"></script>')
+
+
+# In order to run this notebook, please use the following(note that currently only those with berkeley.edu have access to the following link):
+
+# https://colab.research.google.com/drive/1Y8hSG1i7OO6zf0RMb_2aI9lqBSDp9DuF?usp=sharing
